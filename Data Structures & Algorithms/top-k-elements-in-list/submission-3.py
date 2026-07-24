8c7a82e478bc1472ae21835ac8156695e45f40f7
Daniel Silva <122170335+danibapsilva@users.noreply.github.com>
2026-07-24 14:42:37 -0400

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
@@ -0,0 +1,35 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # INTUITIVE APPROACH
+            # count = {}
+            # for i in nums:
+            #     count[i] = count.get(i, 0) + 1
+            
+            # return sorted(count, key=lambda x: count[x], reverse=True)[:k]
+
+        # MIN-HEAP approach -> heapq sorted with min @indx 0
+        freq = {}
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+        
+
+        heap = []
+        for num in freq: # keys
+            heapq.heappush(heap, (freq[num], num)) # sort heap by freq
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1]) # tuple (freq, num) so index [1] grabs num
+        
+        return res
+
+        # BUCKET-SORT APPROACH -> index is the 0-n, n is len, with the value being the freq
+        # freq = {}
+        # for num in nums:
+        #     freq[num] = freq.get(num, 0) + 1
+        
+        # buckets = []
+        # for num in freq.keys():
+
