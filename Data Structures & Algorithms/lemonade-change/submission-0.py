d57c10d4d0251a4b7efaabd3b403fa65c892eeab
Daniel Silva <122170335+danibapsilva@users.noreply.github.com>
2026-08-12 21:56:44 -0400

    Add: lemonade-change - submission-0

---
+++ Data Structures & Algorithms/lemonade-change/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        fives = tens = 0
+        for bill in bills:
+            if bill == 5:
+                fives += 1
+                continue
+            elif bill == 10:
+                if not fives:
+                    return False
+                fives -= 1
+                tens += 1
+            else: # $20 bill
+                if not fives:
+                    print("s")
+                    return False
+                if tens: # fives and tens
+                    fives -= 1
+                    tens -= 1
+                else:
+                    if fives < 3:
+                        print("asd")
+                        return False
+                    fives -= 3
+
+        
+        return True
