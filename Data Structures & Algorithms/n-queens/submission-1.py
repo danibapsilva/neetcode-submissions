a63bcec7470d2f2fbcc3d636c7a349391200dd46
Daniel Silva <122170335+danibapsilva@users.noreply.github.com>
2026-08-25 10:45:18 -0400

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        cols, negDiags, posDiags = set(), set(), set()
+
+        res, positions = [], []
+        def backtrack(row):
+            if row == n:
+                res.append(positions.copy())
+                return
+            
+            for col in range(n):
+                if col in cols or row + col in posDiags or row - col in negDiags:
+                    continue
+
+                placement = '.' * n
+                placement = placement[:col] + 'Q' + placement[col + 1:]
+
+                cols.add(col)
+                posDiags.add(row + col)
+                negDiags.add(row - col)
+                positions.append(placement)
+
+                backtrack(row + 1)
+
+                cols.remove(col)
+                posDiags.remove(row + col)
+                negDiags.remove(row - col)
+                positions.pop()
+
+        backtrack(0)
+        return res
