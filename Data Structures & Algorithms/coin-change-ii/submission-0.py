42c6a0afdaa60d98d121bf27c50512e45ea7bed9
Daniel Silva <122170335+danibapsilva@users.noreply.github.com>
2026-09-24 10:54:27 -0400

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for coin in coins:
+            for a in range(1, amount + 1):
+                dp[a] += dp[a - coin] if coin <= a else 0
+        return dp[amount]
